e5d1c922dda8975fa8593fbfda9231939950f90e
Oluseyi Adedokun <oluseyi.adedokun@paycurra.com>
2025-07-25 08:35:39 +0100

    refresh

---
+++ src/api/pyc-menu-bar/controllers/pyc-menu-bar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pyc-menu-bar controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pyc-menu-bar.pyc-menu-bar');
---
+++ src/api/visual-merchandising-layout/services/visual-merchandising-layout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * visual-merchandising-layout service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::visual-merchandising-layout.visual-merchandising-layout');
