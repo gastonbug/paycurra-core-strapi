e61d9e4b1cc29ac66b2b6df5b25cdb85d877b7c4
Kwesi Hervie <herviek2001@gmail.com>
2025-09-25 13:55:19 +0100

    removed unnecessary comments

--- config/middlewares.js
+++ config/middlewares.js
@@ -12,25 +12,15 @@ module.exports = [
             "'self'",
             "data:",
             "blob:",
-            "dl.airtable.com", // Required for Strapi < 4.10.6, you can remove it otherwise
-            "https://market-assets.strapi.io", // Required for Strapi >= 4.10.6, you can remove it otherwise
-            /**
-             * Note: If using a STORAGE_URL replace
-             * `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
-             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
-             */
+            "dl.airtable.com",
+            "https://market-assets.strapi.io",
             `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
           ],
           "media-src": [
             "'self'",
             "data:",
             "blob:",
-            "dl.airtable.com", // Required for Strapi < 4.10.6, you can remove it otherwise
-            /**
-             * Note: If using a STORAGE_URL replace
-             * `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
-             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
-             */
+            "dl.airtable.com",
             `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
           ],
           upgradeInsecureRequests: null,
